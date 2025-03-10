863986d650ff69cb3e0c98b7e1ac7d1335f27be2
Aleksandr <aik5787@gmail.com>
2025-03-10 10:39:13 -0400

    cicd

--- cypress.config.js
+++ cypress.config.js
@@ -18,15 +18,6 @@ module.exports = defineConfig({
       require('@cypress/grep/src/plugin')(config);
       config.env.USER_EMAIL = process.env.USER_EMAIL;
       config.env.USER_PASSWORD = process.env.USER_PASSWORD;
-      // on('before:browser:launch', (browser, launchOptions) => {
-      //   if (browser.name === 'firefox') {
-      //     launchOptions.preferences['browser.cache.disk.enable'] = false;
-      //     launchOptions.preferences['browser.cache.memory.enable'] = false;
-      //     launchOptions.preferences['browser.cache.offline.enable'] = false;
-      //     launchOptions.preferences['network.http.use-cache'] = false;
-      //   }
-      //   return launchOptions;
-      // });
       return config;
     },
   },
